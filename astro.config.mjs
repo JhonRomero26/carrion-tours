203c3027c7c4750a01047c26e7aa9f982176ec55
Jhon A. Romero Gonzaga <romerogonzaga21@gmail.com>
2024-01-13 11:43:15 -0500

    chore: add server vercel support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
 import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   integrations: [
     icon({
       include: {
